97f913c5aefd7d02c8479e5cd916f0aaf17d8250
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-01-16 14:33:28 -0300

    Get Tarefas por Id

--- src/main/java/br/com/gerenciamento_tarefas/controller/TarefasController.java
+++ src/main/java/br/com/gerenciamento_tarefas/controller/TarefasController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +40,15 @@ public class TarefasController {
 			return ResponseEntity.status(404).build();
 		}
 		return ResponseEntity.ok(tarefas);
+	}
+	
+	@GetMapping("/tarefas/{cod}")
+	public ResponseEntity<Tarefas> tarefaPorId(@PathVariable long cod){
+		
+		Tarefas tarefa = dao.findById(cod).orElse(null);
+		if(tarefa==null) {
+			return ResponseEntity.status(404).build();
+		}
+		return ResponseEntity.ok(tarefa);
 	}
 }
