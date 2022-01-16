82817a749fa596ceaaea134df85098fdcea7b300
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-01-16 14:28:52 -0300

    GetAll tarefas

--- src/main/java/br/com/gerenciamento_tarefas/controller/TarefasController.java
+++ src/main/java/br/com/gerenciamento_tarefas/controller/TarefasController.java
@@ -1,7 +1,10 @@
 package br.com.gerenciamento_tarefas.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,4 +31,13 @@ public class TarefasController {
 		}
 	}
 	
+	@GetMapping("/tarefas")
+	public ResponseEntity<List<Tarefas>> todasTarefas() {
+		List<Tarefas> tarefas = (List<Tarefas>) dao.findAll();
+		
+		if(tarefas.size() == 0) {
+			return ResponseEntity.status(404).build();
+		}
+		return ResponseEntity.ok(tarefas);
+	}
 }
