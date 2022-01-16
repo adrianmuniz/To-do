fb7f7555c885134f45857fa896b8206668febb71
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-01-16 12:17:48 -0300

    Entidades

---
+++ src/main/java/br/com/gerenciamento_tarefas/entities/Tarefas.java
@@ -0,0 +1,51 @@
+package br.com.gerenciamento_tarefas.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Tarefas")
+public class Tarefas {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name="id")
+	private long id;
+	
+	@Column(name="descricao")
+	private String descricao;
+	
+	@Column(name="prioridades")
+	private String prioridades;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getPrioridades() {
+		return prioridades;
+	}
+
+	public void setPrioridades(String prioridades) {
+		this.prioridades = prioridades;
+	}
+	
+	
+	
+}
--- src/main/java/br/com/gerenciamento_tarefas/entities/Usuario.java
+++ src/main/java/br/com/gerenciamento_tarefas/entities/Usuario.java
@@ -1,5 +1,61 @@
 package br.com.gerenciamento_tarefas.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Usuario")
 public class Usuario {
 
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name="Id")
+	private long id;
+	
+	@Column(name="nome", nullable = true)
+	private String nome;
+	
+	@Column(name="email", nullable = true)
+	private String email;
+	
+	@Column(name="senha", nullable = true)
+	private String senha;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
 }
